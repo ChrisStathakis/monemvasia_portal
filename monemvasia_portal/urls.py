6863cb688b4bf4b82ac71b6c3a13a0b8b4574e88
Zefarak <lirageika@hotmail.gr>
2021-10-10 16:28:02 +0300

    fdtr

--- monemvasia_portal/urls.py
+++ monemvasia_portal/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', include('frontend.urls')),
 
     path('αγγελιες/', include('jobPostings.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
